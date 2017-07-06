8e61645ee49a2ff7de2c4f1cc47cdea45f2f16d6
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-07-06 16:48:01 -0400

    updated version of game.js file, working on generate random number function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,13 @@
-var ranNum = [Math.floor(Math.random() * 12)];
-alert(ranNum);
+
+
+//function generateRandomNumber () {
+  //var ranNum = [Math.floor(Math.random() * 12)];
+  //$('.gem1').append('<span>' + ranNum + '</span>'); failed code attempting to append random number to gem1 div
+  
+  function genRanNum () {
+    var ranNum = [Math.floor(Math.random() * 12)];
+  }
+
+  
+ $('.gem1').on('click', genRanNum);
+
