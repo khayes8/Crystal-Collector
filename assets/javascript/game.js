193357020281e1b273faf721d0e5794141ffc7c5
Kelsey Hayes <kelseyhayes@Kelseys-MacBook-Pro.local>
2017-07-06 22:00:33 -0400

    This is my updated version of game.js file, rannum now works when clicking on a gem.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,16 @@
 //function generateRandomNumber () {
   //var ranNum = [Math.floor(Math.random() * 12)];
   //$('.gem1').append('<span>' + ranNum + '</span>'); failed code attempting to append random number to gem1 div
-  
+  $( document ).ready(function() {
+
   function genRanNum () {
     var ranNum = [Math.floor(Math.random() * 12)];
+    
+    alert(ranNum);
   }
 
   
  $('.gem1').on('click', genRanNum);
+
+  });
 
